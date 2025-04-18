e06e2877679fc33c15413aedffe1cb1ecc5a78b1
Nazar Khlivnyi <135509585+NazarKhl@users.noreply.github.com>
2025-04-18 09:16:09 +0200

    Innit second web api

---
+++ backend.WebAPI2.0/webapi2/src/main/java/com/java/webapi2/Webapi2Application.java
@@ -0,0 +1,13 @@
+package com.java.webapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapi2Application.class, args);
+	}
+
+}
---
+++ backend.WebAPI2.0/webapi2/src/test/java/com/java/webapi2/Webapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.webapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
